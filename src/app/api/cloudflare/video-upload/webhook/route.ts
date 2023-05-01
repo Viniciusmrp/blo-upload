26f714e715282b05e5e5526b547704db6deeda31
Rodolpho Bravo <bravorodolpho@gmail.com>
2023-05-01 18:19:46 -0400

    fix

--- src/app/api/cloudflare/video-upload/webhook/route.ts
+++ src/app/api/cloudflare/video-upload/webhook/route.ts
@@ -1,69 +1,69 @@
-import { prisma } from '@/db'
+// import { prisma } from '@/db'
 
 export const config = {
   api: {
     bodyParser: false,
   },
-}
+};
 
 type VideoData = {
-  uid: string
-  creator: null
-  thumbnail: string
-  thumbnailTimestampPct: number
-  readyToStream: boolean
+  uid: string;
+  creator: null;
+  thumbnail: string;
+  thumbnailTimestampPct: number;
+  readyToStream: boolean;
   status: {
-    state: string
-    step: string
-    pctComplete: string
-    errorReasonCode: string
-    errorReasonText: string
-  }
+    state: string;
+    step: string;
+    pctComplete: string;
+    errorReasonCode: string;
+    errorReasonText: string;
+  };
   meta: {
-    filename: string
-    filetype: string
-    name: string
-    type: string
-  }
-  created: string
-  modified: string
-  size: number
-  preview: string
-  allowedOrigins: []
-  requireSignedURLs: boolean
-  uploaded: string
-  uploadExpiry: string
-  maxSizeBytes: null
-  maxDurationSeconds: number
-  duration: number
+    filename: string;
+    filetype: string;
+    name: string;
+    type: string;
+  };
+  created: string;
+  modified: string;
+  size: number;
+  preview: string;
+  allowedOrigins: [];
+  requireSignedURLs: boolean;
+  uploaded: string;
+  uploadExpiry: string;
+  maxSizeBytes: null;
+  maxDurationSeconds: number;
+  duration: number;
   input: {
-    width: number
-    height: number
-  }
+    width: number;
+    height: number;
+  };
   playback: {
-    hls: string
-    dash: string
-  }
-  watermark: null
-  clippedFrom: null
-  publicDetails: null
-}
+    hls: string;
+    dash: string;
+  };
+  watermark: null;
+  clippedFrom: null;
+  publicDetails: null;
+};
 
 export async function POST(request: Request) {
-  const data = (await request.json()) as VideoData
+  const data = (await request.json()) as VideoData;
 
-  console.log('Cloudflare Webhook Run', new Date(), data)
+  console.log("Cloudflare Webhook Run", new Date(), data);
 
   try {
-    await prisma.cloudflareVideo.update({
-      where: {
-        id: data.meta.filename,
-      },
-      data: {
-        videoIdOnCloudflare: data.uid,
-      },
-    })
+    // await prisma.cloudflareVideo.update({
+    //   where: {
+    //     id: data.meta.filename,
+    //   },
+    //   data: {
+    //     videoIdOnCloudflare: data.uid,
+    //   },
+    // })
   } catch (e) {
-    console.log('ERROR', e)
+    console.log("ERROR", e);
   }
 }
