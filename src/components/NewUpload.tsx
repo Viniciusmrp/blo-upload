ee0caefcc4f6084412e62f13dffcc99fc12b4c37
Vinicius <viniciusmrp@gmail.com>
2025-01-06 17:26:39 -0300

    I have removed the logging of the Signed URL for protection

--- src/components/NewUpload.tsx
+++ src/components/NewUpload.tsx
@@ -56,7 +56,7 @@ const NewUpload = () => {
           "Content-Type": "video/mp4",
         },
       });
-      console.log("Signed URL:", signedUrl);
+      
       console.log("Video uploaded successfully");
 
       setIsUploading(false);
