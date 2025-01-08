90b2a4ad406b44674d91791babd4a2643a59d0f8
Vinicius <viniciusmrp@gmail.com>
2025-01-08 13:39:46 -0300

    Fixed Project ID environment variables

--- src/components/NewUpload.tsx
+++ src/components/NewUpload.tsx
@@ -21,7 +21,7 @@ import { initializeApp } from "firebase/app";
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.GCP_PROJECT_ID, // Using the GCP_PROJECT_ID for consistency
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
