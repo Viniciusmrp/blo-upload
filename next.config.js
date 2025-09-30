24e9dcd245249535e43f0b4ca378e13a52de6c0b
viniciusmrp <viniciusmrp@gmail.com>
2025-09-30 16:39:25 +0000

    fixing deployment bug

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   // ADD THIS WEBPACK CONFIG
   webpack: (config, { isServer }) => {
     if (!isServer) {
